2eaafc62cb186cceb846c0d96420946dfc0fe5c5
Oscar Sanz <osanzce@gmail.com>
2022-04-17 01:39:31 +0200

    adds generateRefreshToken middleware

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -2,17 +2,17 @@ const bcrypt = require('bcryptjs');
 const { v4: uuidv4, v4 } = require('uuid');
 const jwt = require('jsonwebtoken');
 const { User, Token } = require('../db');
-const { generateAccessToken } = require('../middlewares/auth.middleware');
+const { generateAccessToken, generateRefreshToken } = require('../middlewares/auth.middleware');
 
 //==========REGISTER============
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
-  if (!name || !email || password) {
+  if (!name || !email || !password) {
     return res.status(422).json({ msg: 'all fields are required' });
   }
   try {
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ where: { email: email }, raw: true });
     if (existingUser) return res.status(400).json({ msg: 'some error occurred, please try again' });
 
     const hash = await bcrypt.hash(password, 10);
@@ -32,6 +32,7 @@ const register = async (req, res) => {
 //===============LOGIN==============
 
 const logIn = async (req, res, next) => {
+  console.log('aqui');
   const { email, password } = req.body;
   if (!email || !password) {
     return res.status(422).json({ msg: 'all fields are required, please try again' });
@@ -48,15 +49,23 @@ const logIn = async (req, res, next) => {
       return res.status(403).json({ msg: 'wrong credentials, please try again' });
     }
 
-    const accessToken = generateAccessToken(user);
+    const accessToken = generateAccessToken(user.email);
 
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_KEY);
+    const refreshToken = generateRefreshToken(user.email);
+
     const id = v4();
+
     const newRefreshToken = await Token.create({
-      id,
+      t_id: id,
       token: refreshToken,
+      user_id: user.id,
     });
-    return res.json({ accessToken, refreshToken });
+    return res
+      .cookie('token', refreshToken, {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      })
+      .json({ accessToken, user });
   } catch (error) {
     return res.status(500).json({ msg: error });
   }
@@ -65,18 +74,21 @@ const logIn = async (req, res, next) => {
 //==============NEW ACCESS TOKEN====================
 
 const newAccessToken = async (req, res, next) => {
-  const refreshToken = req.body.token;
-  if (refreshToken === null) return res.status(401).json({ msg: 'unauthorized' });
-  isValidToken = await Token.findOne({
+  const refreshToken = req.cookies?.token;
+
+  if (!refreshToken) return res.status(401).json({ msg: 'unauthorized' });
+
+  const isValidToken = await Token.findOne({
     where: {
       token: refreshToken,
     },
     raw: true,
   });
-  if (!isValidToken) return res.status(403).json({ msg: 'invalid token' });
+  if (!isValidToken) return res.status(403).json({ msg: 'no token in db' });
+
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_KEY, (err, user) => {
     if (err) return res.status(403).json({ msg: 'invalid token' });
-    const accessToken = generateAccessToken({ id: user.name });
+    const accessToken = generateAccessToken(user.email);
     return res.status(200).json({ accessToken });
   });
 };
@@ -84,7 +96,9 @@ const newAccessToken = async (req, res, next) => {
 //=============LOGOUT==========================
 
 const logOut = async (req, res, next) => {
-  const { token } = req.body;
+  const token = req.cookies.token;
+  if (!token) return res.status(204);
+
   const existingToken = await Token.findOne({
     where: {
       token,
@@ -93,14 +107,16 @@ const logOut = async (req, res, next) => {
   });
 
   if (!existingToken) return res.status(404).json({ msg: 'impossible to logout, please try again' });
+
+  console.log('exisiting Token', existingToken);
   try {
-    const deletedToken = await Token.destroy({
+    await Token.destroy({
       where: {
-        id: existingToken.id,
+        t_id: existingToken.t_id,
       },
       raw: true,
     });
-    return res.status(200).json({ msg: 'you are logged out' });
+    return res.clearCookie('token', { httpOnly: true }).status(200).json({ msg: 'you are logged out' }); // on production secure true -- only serves in https
   } catch (error) {
     return res.status(500).json({ msg: error });
   }
