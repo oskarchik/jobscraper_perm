0a9580e7eb50781b9e0c9b13cecd2c94614da53b
Oscar Sanz <osanzce@gmail.com>
2022-05-09 19:48:57 +0200

    FIX: url api in production

--- client/job-scraper-app/src/api/axios.js
+++ client/job-scraper-app/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8000';
+const BASE_URL =
+  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL_PROD : process.env.REACT_APP_API_URL_DEV;
 
 export default axios.create({
   baseURL: BASE_URL,
