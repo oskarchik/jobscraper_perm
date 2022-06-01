dd2fe0fd597c05f1f2defdc765676c6763c0e546
Oscar Sanz <osanzce@gmail.com>
2022-06-01 19:06:25 +0200

    FIX: removes engine warning

--- server/src/middlewares/cache.middleware.js
+++ server/src/middlewares/cache.middleware.js
@@ -3,6 +3,7 @@ const getExpeditiousCache = require('express-expeditious');
 const defaultOptions = {
   namespace: 'expresscache',
   defaultTtl: '10 seconds',
+  engine: require('expeditious-engine-memory')(),
 };
 
 const cacheInit = getExpeditiousCache(defaultOptions);
