818b68c1d9226fef77d0801068b434c3a09892a0
Oscar Sanz <osanzce@gmail.com>
2022-06-03 19:13:38 +0200

    FIX: fix nodemailer error with gmail transport

--- server/src/cronJob/index.js
+++ server/src/cronJob/index.js
@@ -6,12 +6,12 @@ const remoteJobsCron = async () => {
   fetchCron.start();
   sendMailCron.start();
 };
-
-const fetchCron = new CronJob('0 10,17 * * *', async () => {
+// 0 10,17 * * *
+const fetchCron = new CronJob('* * * * *', async () => {
   await fetchJobs();
 });
-
-const sendMailCron = new CronJob('10 17 * * *', async () => {
+// 10 17 * * *
+const sendMailCron = new CronJob('* * * * *', async () => {
   console.log('sending email');
   await latestJobs(process.env.MOCK_EMAIL, process.env.MOCK_PASSWORD);
 });
--- server/src/mailer/nodemailer.js
+++ server/src/mailer/nodemailer.js
@@ -1,32 +1,73 @@
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+
+// These id's and secrets should come from .env file.
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REFRESH_TOKEN, INBOX_EMAIL } = process.env;
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+
+const oAuth2Client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, REDIRECT_URI);
+oAuth2Client.setCredentials({ refresh_token: GOOGLE_REFRESH_TOKEN });
 
 async function sendMail(data) {
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    service: 'gmail',
-    secure: true,
-    auth: {
-      user: process.env.INBOX_EMAIL,
-      pass: process.env.INBOX_PASSWORD,
-    },
-  });
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
 
-  let info = transporter.sendMail(
-    {
+    const transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'osanzce@gmail.com',
+        clientId: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        refreshToken: GOOGLE_REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
+
+    const mailOptions = {
       from: `"Oscar " <${process.env.INBOX_EMAIL}>`,
-      to: process.env.INBOX_EMAIL,
-      subject: `Job offers fot today ${new Date().toLocaleDateString()}`,
+      to: INBOX_EMAIL,
+      subject: `Today's job offers ${new Date().toLocaleDateString()}`,
       text: 'These are the job collected today from your database!!!',
       html: data,
-    },
-    function (err, info) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('success');
-      }
-    }
-  );
+    };
+
+    const result = await transport.sendMail(mailOptions);
+    console.log('success');
+    return result;
+  } catch (error) {
+    console.log('error in sendmail', error);
+    return error;
+  }
 }
+
+// async function sendMail(data) {
+//   let transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     service: 'gmail',
+//     secure: true,
+//     auth: {
+//       user: process.env.INBOX_EMAIL,
+//       pass: process.env.INBOX_PASSWORD,
+//     },
+//   });
+
+//   let info = transporter.sendMail(
+//     {
+//       from: `"Oscar " <${process.env.INBOX_EMAIL}>`,
+//       to: process.env.INBOX_EMAIL,
+//       subject: `Job offers fot today ${new Date().toLocaleDateString()}`,
+//       text: 'These are the job collected today from your database!!!',
+//       html: data,
+//     },
+//     function (err, info) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log('success');
+//       }
+//     }
+//   );
+// }
 
 module.exports = { sendMail };
