b603d29bb03518ff1be113fed25a8b485e1395f2
Oscar Sanz <osanzce@gmail.com>
2022-09-12 19:22:24 +0200

    add notification to Table page

--- client/job-scraper-app/src/pages/Table/TablePage.jsx
+++ client/job-scraper-app/src/pages/Table/TablePage.jsx
@@ -1,8 +1,11 @@
 import { StyledTAblePage } from './TablePage.styled';
-import { Table, SearchBar } from '../../components';
+import { Table, SearchBar, Toast } from '../../components';
+import { useToast } from '../../hooks';
 const TablePage = ({ title }) => {
+  const { notification } = useToast();
   return (
     <StyledTAblePage className='all-jobs'>
+      {notification?.response && <Toast />}
       <h2 className='title'>{title}</h2>
       <SearchBar />
       <Table />
