4f32e006dd939c3ac38d900037639e994de3eb7a
Oscar Sanz <osanzce@gmail.com>
2022-09-17 11:45:46 +0200

    add timeout to toasts to fix error after wrong attemp

--- client/job-scraper-app/src/context/toastContext.jsx
+++ client/job-scraper-app/src/context/toastContext.jsx
@@ -1,9 +1,16 @@
+import { useEffect } from 'react';
 import { createContext, useState } from 'react';
 
 const ToastContext = createContext();
 
 export const ToastProvider = ({ children }) => {
   const [notification, setNotification] = useState();
+  useEffect(() => {
+    if (notification) {
+      const timer = setTimeout(() => setNotification(), 3500);
+      return () => clearTimeout(timer);
+    }
+  }, [notification]);
 
   return <ToastContext.Provider value={{ notification, setNotification }}>{children}</ToastContext.Provider>;
 };
